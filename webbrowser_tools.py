7b18cc1c57562b15abe9e02df0a73a7ed5481ef7
pythonestasocretes69 <93080394+pythonestasocretes69@users.noreply.github.com>
2023-06-22 00:40:50 +0530

    Create webbrowser_tools.py

---
+++ webbrowser_tools.py
@@ -0,0 +1,40 @@
+import webbrowser 
+
+def getweb(address):
+    # register all browsers we have
+
+    ##1 brave
+    brave_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
+    if 'brave' not in webbrowser._browsers:
+        webbrowser.register('brave', None, webbrowser.BackgroundBrowser(brave_path))
+    ##2 Crome
+    crome_path = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+    if 'crome' not in webbrowser._browsers:
+        webbrowser.register('crome', None, webbrowser.BackgroundBrowser(crome_path))
+    ##3 Firefox
+    fox_path = "‪C:\Program Files\Mozilla Firefox\firefox.exe"
+    if 'firefox' not in webbrowser._browsers:
+        webbrowser.register('firefox', None, webbrowser.BackgroundBrowser(fox_path))
+    ##4 Explorer
+    edge_path ="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+    if 'explorer' not in webbrowser._browsers:
+        webbrowser.register('edge', None, webbrowser.BackgroundBrowser(edge_path))
+
+    #Menu
+    option = int(input('''Enter the browser you want to use
+1-> Brave
+2-> Crome
+3-> Firefox <not fuctional right now>
+4-> Explorer\n'''))
+    if option == 1:
+        webbrowser.get('brave').open(address)
+    elif option == 2:
+        webbrowser.get('crome').open(address)
+    elif option == 3:
+        webbrowser.get('firefox').open(address)
+    elif option == 4:
+        webbrowser.get('edge').open(address)
+    else:
+        webbrowser.get('brave').open(address)
+
+    #opening the address
