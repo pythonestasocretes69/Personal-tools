962ecd92de4b2db2094776e8d737131afa953871
pythonestasocretes69 <93080394+pythonestasocretes69@users.noreply.github.com>
2023-06-23 18:30:12 +0530

    added predefined action

--- webbrowser_tools.py
+++ webbrowser_tools.py
@@ -1,6 +1,6 @@
 import webbrowser 
 
-def getweb(address):
+def getweb(address , option = None):
     # register all browsers we have
 
     ##1 brave
@@ -14,18 +14,21 @@ def getweb(address):
     ##3 Firefox
     fox_path = "‪C:\Program Files\Mozilla Firefox\firefox.exe"
     if 'firefox' not in webbrowser._browsers:
-        webbrowser.register('firefox', None, webbrowser.BackgroundBrowser(fox_path))
+        webbrowser.register('firefox', None, webbrowser.GenericBrowser('firefox'), preferred=True)
     ##4 Explorer
     edge_path ="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
     if 'explorer' not in webbrowser._browsers:
         webbrowser.register('edge', None, webbrowser.BackgroundBrowser(edge_path))
 
     #Menu
-    option = int(input('''Enter the browser you want to use
+    if option != None:
+        print(option, 'is the option chosen')
+    else:
+        option = int(input('''Enter the browser you want to use
 1-> Brave
 2-> Crome
 3-> Firefox <not fuctional right now>
-4-> Explorer\n'''))
+4-> Explorer\n#'''))
     if option == 1:
         webbrowser.get('brave').open(address)
     elif option == 2:
@@ -36,5 +39,3 @@ def getweb(address):
         webbrowser.get('edge').open(address)
     else:
         webbrowser.get('brave').open(address)
-
-    #opening the address
