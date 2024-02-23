e83b6a515185e8d5c7346c2b7e948f418e6c8159
pythonestasocretes69 <93080394+pythonestasocretes69@users.noreply.github.com>
2024-02-23 23:38:42 +0530

    simplification

--- Bots/Ask_Google.py
+++ Bots/Ask_Google.py
@@ -3,6 +3,9 @@ import time
 file = open('topics.txt','r')
 lines = file.readlines()
 for i in lines:
-    webbrowser.open('https://www.google.com/search?q='+i)
+    if (i.startswith('http')):
+        webbrowser.open(i)
+    else:
+        webbrowser.open('https://www.google.com/search?q='+i)
     time.sleep(5)
 file.close()
---
+++ Bots/Ask_anyone.py
@@ -0,0 +1,19 @@
+import webbrowser as web
+import pyautogui as auto
+import time
+file = open('topics.txt','r')
+lines = file.readlines()
+for i in lines:
+    stop = input('Press Enter')
+    if i.startswith("g"):
+        web.open("https://google.com")
+        web.typewrite("/")
+    elif i.startswith("b"):
+        web.open("https://bard.com")
+    elif i.startswith("c"):
+        web.open("https://chat.openai.com")
+    time.sleep(10)
+    auto.typewrite(i[1:])
+    time.sleep(20)
+file.close()
+print(lines)
